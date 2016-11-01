82428efb61c96a5b394ebe6e510a04e51e8a6664
fakang.wfk <fakang.wfk@alibaba-inc.com>
2016-11-01 16:56:38 +0800

    make the http_accounting_id directive support on the location of the if

--- src/ngx_http_accounting_module.c
+++ src/ngx_http_accounting_module.c
@@ -31,7 +31,8 @@ static ngx_command_t  ngx_http_accounting_commands[] = {
       NULL},
 
     { ngx_string("http_accounting_id"),
-      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
+      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_HTTP_LIF_CONF
+                        |NGX_CONF_TAKE1,
       ngx_http_accounting_set_accounting_id,
       NGX_HTTP_LOC_CONF_OFFSET,
       0,
